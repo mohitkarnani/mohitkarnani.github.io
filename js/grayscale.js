ba891902fc880bb65b5864d4bf7680643a3f26f9
mohitkarnani <mohitkarnani@gmail.com>
2015-11-30 00:45:29 -0300

    map

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(-33.4423436, -70.6378606), // FEN
+        center: new google.maps.LatLng(-33.442, -70.637), // FEN
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -169,7 +169,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(-33.4423436, -70.6378606);
+    var myLatLng = new google.maps.LatLng(-33.442, -70.637);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
